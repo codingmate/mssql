01661da14454f951bb46234544cc6fff27ba07a1
sy-playground <87705358+codingmate@users.noreply.github.com>
2023-02-01 13:27:20 +0900

    Create 10.union.sql

---
+++ 10.union.sql
@@ -0,0 +1,17 @@
+                      SELECT 1
+                UNION SELECT 1
+                UNION SELECT 1
+                
+                /*
+                  1
+                */
+                      SELECT 1
+            UNION ALL SELECT 1
+            UNION ALL SELECT 1
+                /*
+                  1
+                  1
+                  1
+                */
+                
+                
